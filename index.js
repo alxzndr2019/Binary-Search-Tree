88a344f195207fe1d69a488da66387cd0251ba23
Alex Ohioze Omiunu <45854856+alxzndr2019@users.noreply.github.com>
2021-11-14 22:50:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+let num = [2,7,8,10,11,12,16,20,16]
+
+function BinarySearch (array,target){
+  left = 0
+  right = array.length - 1
+
+  while(left<right){
+    let mid = Math.floor((left+right)/2)
+    if (target === array[mid]){
+      return mid
+    }else if(target < array[mid]){
+      right = mid - 1;
+    }else{
+      left = mid + 1;
+    }
+  }
+  return false
+}
+
+console.log(BinarySearch(num,12))
